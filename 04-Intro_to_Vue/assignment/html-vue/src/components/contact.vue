bee7289799200432a730fceadd9cc6efd8ba2d20
Ljubica S <lsmile2@uw.edu>
2024-09-16 21:07:12 -0700

    adding assignment 4 - vue.js

---
+++ 04-Intro_to_Vue/assignment/html-vue/src/components/contact.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+const name = ref ("contact");
+const ingredients = ref(['Email: alex@analyticsltd.com', 'Phone: 720-240-8431', 'Address: 17483 E Temple Dr, Aurora, CO 80015']);
+</script>
+
+<template>
+  <div class="contact">
+    <h1>Contact Page</h1>
+    <main>
+        <ul>
+            <li v-for='ingredient in ingredients' :key='ingredient'>
+                {{ ingredient }}
+            </li>
+        </ul>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.contact {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
---
+++ 04-Intro_to_Vue/assignment/html-vue/src/components/footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <h3>{{ copyright }}</h3>
+  </footer>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        copyright:'Copyright & IT Consulting 2024'
+      }
+    }
+  }
+</script>
+
+//we use scoped, so this styles will apply only to this component
+<style scoped>
+  footer {
+    background: #eceeef;
+    padding: 6px;
+  }
+  p{
+    color: #222;;
+    text-align: center;
+  }
+</style>

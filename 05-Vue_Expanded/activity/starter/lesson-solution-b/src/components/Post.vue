2a758aa6e1c8c09a72972824255a12217807be04
Ljubica S <lsmile2@uw.edu>
2024-09-19 23:53:33 -0700

    Working on component activity lesson 05

---
+++ 05-Vue_Expanded/activity/starter/lesson-solution-b/src/components/Post.vue
@@ -0,0 +1,35 @@
+<script setup>
+    import { defineProps, ref } from 'vue'
+
+    const props = defineProps({
+        id: Number,
+        author: String,
+        title: String,
+        likes: Number,
+        category: String,
+        excerpt: String
+    })
+
+    const likes = ref(props.likes)
+
+    const incerementLikes = () => {
+        likes.value++
+    }
+</script>
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <p>By: {{ author }}</p>
+        <p>Category: {{ category }}</p>
+        <p>{{ excerpt }}</p>
+        <p>Likes: {{ likes }}</p>
+        <button @click="incerementLikes">Like</button>
+    </div>
+</template>
+<style scoped>
+.post{
+    border: 1px solid #ccc;
+    padding: 1rem;
+    margin: 1rem 0;
+}
+</style>
